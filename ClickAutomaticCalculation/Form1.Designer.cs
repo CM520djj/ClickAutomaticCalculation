0d4018f0914def466214937d9628447956704e5f
Cmorton <Cmorton@DESKTOP-A75VL7D>
2023-12-26 09:00:02 +0800

    添加项目文件。

---
+++ ClickAutomaticCalculation/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace ClickAutomaticCalculation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.uiCheckBox1 = new Sunny.UI.UICheckBox();
+            this.uiCheckBox2 = new Sunny.UI.UICheckBox();
+            this.uiCheckBox3 = new Sunny.UI.UICheckBox();
+            this.uiCheckBox4 = new Sunny.UI.UICheckBox();
+            this.uiCheckBox5 = new Sunny.UI.UICheckBox();
+            this.uiCheckBox6 = new Sunny.UI.UICheckBox();
+            this.uiCheckBox7 = new Sunny.UI.UICheckBox();
+            this.uiCheckBox8 = new Sunny.UI.UICheckBox();
+            this.uiCheckBox9 = new Sunny.UI.UICheckBox();
+            this.uiCheckBox10 = new Sunny.UI.UICheckBox();
+            this.uiCheckBox11 = new Sunny.UI.UICheckBox();
+            this.uiCheckBox12 = new Sunny.UI.UICheckBox();
+            this.uibtSumit = new Sunny.UI.UIButton();
+            this.uitxtSum = new Sunny.UI.UITextBox();
+            this.uiLabel1 = new Sunny.UI.UILabel();
+            this.uibtSelectedAll = new Sunny.UI.UIButton();
+            this.uibtCancelAll = new Sunny.UI.UIButton();
+            this.SuspendLayout();
+            // 
+            // uiCheckBox1
+            // 
+            this.uiCheckBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiCheckBox1.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiCheckBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.uiCheckBox1.Location = new System.Drawing.Point(59, 52);
+            this.uiCheckBox1.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiCheckBox1.Name = "uiCheckBox1";
+            this.uiCheckBox1.Size = new System.Drawing.Size(72, 29);
+            this.uiCheckBox1.TabIndex = 0;
+            this.uiCheckBox1.Text = "1";
+            // 
+            // uiCheckBox2
+            // 
+            this.uiCheckBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiCheckBox2.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiCheckBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.uiCheckBox2.Location = new System.Drawing.Point(59, 116);
+            this.uiCheckBox2.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiCheckBox2.Name = "uiCheckBox2";
+            this.uiCheckBox2.Size = new System.Drawing.Size(72, 29);
+            this.uiCheckBox2.TabIndex = 1;
+            this.uiCheckBox2.Text = "2";
+            // 
+            // uiCheckBox3
+            // 
+            this.uiCheckBox3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiCheckBox3.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiCheckBox3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.uiCheckBox3.Location = new System.Drawing.Point(59, 180);
+            this.uiCheckBox3.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiCheckBox3.Name = "uiCheckBox3";
+            this.uiCheckBox3.Size = new System.Drawing.Size(72, 29);
+            this.uiCheckBox3.TabIndex = 2;
+            this.uiCheckBox3.Text = "3";
+            // 
+            // uiCheckBox4
+            // 
+            this.uiCheckBox4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiCheckBox4.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiCheckBox4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.uiCheckBox4.Location = new System.Drawing.Point(59, 244);
+            this.uiCheckBox4.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiCheckBox4.Name = "uiCheckBox4";
+            this.uiCheckBox4.Size = new System.Drawing.Size(72, 29);
+            this.uiCheckBox4.TabIndex = 3;
+            this.uiCheckBox4.Text = "4";
+            // 
+            // uiCheckBox5
+            // 
+            this.uiCheckBox5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiCheckBox5.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiCheckBox5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.uiCheckBox5.Location = new System.Drawing.Point(59, 308);
+            this.uiCheckBox5.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiCheckBox5.Name = "uiCheckBox5";
+            this.uiCheckBox5.Size = new System.Drawing.Size(72, 29);
+            this.uiCheckBox5.TabIndex = 4;
+            this.uiCheckBox5.Text = "5";
+            // 
+            // uiCheckBox6
+            // 
+            this.uiCheckBox6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiCheckBox6.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiCheckBox6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.uiCheckBox6.Location = new System.Drawing.Point(59, 372);
+            this.uiCheckBox6.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiCheckBox6.Name = "uiCheckBox6";
+            this.uiCheckBox6.Size = new System.Drawing.Size(72, 29);
+            this.uiCheckBox6.TabIndex = 5;
+            this.uiCheckBox6.Text = "6";
+            // 
+            // uiCheckBox7
+            // 
+            this.uiCheckBox7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiCheckBox7.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiCheckBox7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.uiCheckBox7.Location = new System.Drawing.Point(193, 52);
+            this.uiCheckBox7.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiCheckBox7.Name = "uiCheckBox7";
+            this.uiCheckBox7.Size = new System.Drawing.Size(72, 29);
+            this.uiCheckBox7.TabIndex = 6;
+            this.uiCheckBox7.Text = "11";
+            // 
+            // uiCheckBox8
+            // 
+            this.uiCheckBox8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiCheckBox8.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiCheckBox8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.uiCheckBox8.Location = new System.Drawing.Point(193, 116);
+            this.uiCheckBox8.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiCheckBox8.Name = "uiCheckBox8";
+            this.uiCheckBox8.Size = new System.Drawing.Size(72, 29);
+            this.uiCheckBox8.TabIndex = 7;
+            this.uiCheckBox8.Text = "12";
+            // 
+            // uiCheckBox9
+            // 
+            this.uiCheckBox9.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiCheckBox9.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiCheckBox9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.uiCheckBox9.Location = new System.Drawing.Point(193, 180);
+            this.uiCheckBox9.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiCheckBox9.Name = "uiCheckBox9";
+            this.uiCheckBox9.Size = new System.Drawing.Size(72, 29);
+            this.uiCheckBox9.TabIndex = 8;
+            this.uiCheckBox9.Text = "13";
+            // 
+            // uiCheckBox10
+            // 
+            this.uiCheckBox10.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiCheckBox10.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiCheckBox10.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.uiCheckBox10.Location = new System.Drawing.Point(193, 244);
+            this.uiCheckBox10.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiCheckBox10.Name = "uiCheckBox10";
+            this.uiCheckBox10.Size = new System.Drawing.Size(72, 29);
+            this.uiCheckBox10.TabIndex = 9;
+            this.uiCheckBox10.Text = "14";
+            // 
+            // uiCheckBox11
+            // 
+            this.uiCheckBox11.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiCheckBox11.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiCheckBox11.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.uiCheckBox11.Location = new System.Drawing.Point(193, 308);
+            this.uiCheckBox11.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiCheckBox11.Name = "uiCheckBox11";
+            this.uiCheckBox11.Size = new System.Drawing.Size(72, 29);
+            this.uiCheckBox11.TabIndex = 10;
+            this.uiCheckBox11.Text = "15";
+            // 
+            // uiCheckBox12
+            // 
+            this.uiCheckBox12.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiCheckBox12.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiCheckBox12.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.uiCheckBox12.Location = new System.Drawing.Point(193, 372);
+            this.uiCheckBox12.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiCheckBox12.Name = "uiCheckBox12";
+            this.uiCheckBox12.Size = new System.Drawing.Size(72, 29);
+            this.uiCheckBox12.TabIndex = 11;
+            this.uiCheckBox12.Text = "16";
+            // 
+            // uibtSumit
+            // 
+            this.uibtSumit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uibtSumit.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uibtSumit.Location = new System.Drawing.Point(351, 227);
+            this.uibtSumit.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uibtSumit.Name = "uibtSumit";
+            this.uibtSumit.Size = new System.Drawing.Size(150, 35);
+            this.uibtSumit.TabIndex = 12;
+            this.uibtSumit.Text = "合计";
+            this.uibtSumit.TipsFont = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uibtSumit.Click += new System.EventHandler(this.uibtSumit_Click);
+            // 
+            // uitxtSum
+            // 
+            this.uitxtSum.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.uitxtSum.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uitxtSum.Location = new System.Drawing.Point(351, 180);
+            this.uitxtSum.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.uitxtSum.MinimumSize = new System.Drawing.Size(1, 16);
+            this.uitxtSum.Name = "uitxtSum";
+            this.uitxtSum.Padding = new System.Windows.Forms.Padding(5);
+            this.uitxtSum.ShowText = false;
+            this.uitxtSum.Size = new System.Drawing.Size(150, 29);
+            this.uitxtSum.TabIndex = 13;
+            this.uitxtSum.TextAlignment = System.Drawing.ContentAlignment.MiddleLeft;
+            this.uitxtSum.Watermark = "";
+            // 
+            // uiLabel1
+            // 
+            this.uiLabel1.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.uiLabel1.Location = new System.Drawing.Point(347, 139);
+            this.uiLabel1.Name = "uiLabel1";
+            this.uiLabel1.Size = new System.Drawing.Size(53, 23);
+            this.uiLabel1.TabIndex = 14;
+            this.uiLabel1.Text = "总数";
+            this.uiLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // uibtSelectedAll
+            // 
+            this.uibtSelectedAll.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uibtSelectedAll.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uibtSelectedAll.Location = new System.Drawing.Point(351, 280);
+            this.uibtSelectedAll.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uibtSelectedAll.Name = "uibtSelectedAll";
+            this.uibtSelectedAll.Size = new System.Drawing.Size(59, 35);
+            this.uibtSelectedAll.TabIndex = 15;
+            this.uibtSelectedAll.Text = "全选";
+            this.uibtSelectedAll.TipsFont = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uibtSelectedAll.Click += new System.EventHandler(this.uibtSelectedAll_Click);
+            // 
+            // uibtCancelAll
+            // 
+            this.uibtCancelAll.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uibtCancelAll.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uibtCancelAll.Location = new System.Drawing.Point(442, 282);
+            this.uibtCancelAll.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uibtCancelAll.Name = "uibtCancelAll";
+            this.uibtCancelAll.Size = new System.Drawing.Size(59, 35);
+            this.uibtCancelAll.TabIndex = 16;
+            this.uibtCancelAll.Text = "取消";
+            this.uibtCancelAll.TipsFont = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uibtCancelAll.Click += new System.EventHandler(this.uibtCancelAll_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(586, 450);
+            this.Controls.Add(this.uibtCancelAll);
+            this.Controls.Add(this.uibtSelectedAll);
+            this.Controls.Add(this.uiLabel1);
+            this.Controls.Add(this.uitxtSum);
+            this.Controls.Add(this.uibtSumit);
+            this.Controls.Add(this.uiCheckBox12);
+            this.Controls.Add(this.uiCheckBox11);
+            this.Controls.Add(this.uiCheckBox10);
+            this.Controls.Add(this.uiCheckBox9);
+            this.Controls.Add(this.uiCheckBox8);
+            this.Controls.Add(this.uiCheckBox7);
+            this.Controls.Add(this.uiCheckBox6);
+            this.Controls.Add(this.uiCheckBox5);
+            this.Controls.Add(this.uiCheckBox4);
+            this.Controls.Add(this.uiCheckBox3);
+            this.Controls.Add(this.uiCheckBox2);
+            this.Controls.Add(this.uiCheckBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "选择计算总数";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Sunny.UI.UICheckBox uiCheckBox1;
+        private Sunny.UI.UICheckBox uiCheckBox2;
+        private Sunny.UI.UICheckBox uiCheckBox3;
+        private Sunny.UI.UICheckBox uiCheckBox4;
+        private Sunny.UI.UICheckBox uiCheckBox5;
+        private Sunny.UI.UICheckBox uiCheckBox6;
+        private Sunny.UI.UICheckBox uiCheckBox7;
+        private Sunny.UI.UICheckBox uiCheckBox8;
+        private Sunny.UI.UICheckBox uiCheckBox9;
+        private Sunny.UI.UICheckBox uiCheckBox10;
+        private Sunny.UI.UICheckBox uiCheckBox11;
+        private Sunny.UI.UICheckBox uiCheckBox12;
+        private Sunny.UI.UIButton uibtSumit;
+        private Sunny.UI.UITextBox uitxtSum;
+        private Sunny.UI.UILabel uiLabel1;
+        private Sunny.UI.UIButton uibtSelectedAll;
+        private Sunny.UI.UIButton uibtCancelAll;
+    }
+}
+
---
+++ ClickAutomaticCalculation/Form1.cs
@@ -0,0 +1,59 @@
+﻿using Sunny.UI;
+using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ClickAutomaticCalculation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void uibtSumit_Click(object sender, EventArgs e)
+        {
+            int sum = 0;
+
+            // 遍历所有复选框，判断是否选中，并将选中的数字相加
+            for (int i = 1; i <= 30; i++)
+            {
+                UICheckBox checkBox = Controls.Find("uiCheckBox" + i.ToString(), true).FirstOrDefault() as UICheckBox;
+                if (checkBox != null && checkBox.Checked)
+                {
+                    sum += int.Parse(checkBox.Text);
+                }
+            }
+
+            // 显示合计结果
+            uitxtSum.Text = sum.ToString();
+        }
+
+        private void uibtSelectedAll_Click(object sender, EventArgs e)
+        {
+            // 遍历所有的UICheckBox并将它们的Checked属性设置为true
+            foreach (Control control in this.Controls)
+            {
+                if(control is UICheckBox)
+                {
+                    UICheckBox checkBox = control as UICheckBox;
+                    checkBox.Checked = true;
+                }
+            }
+        }
+
+        private void uibtCancelAll_Click(object sender, EventArgs e)
+        {
+            // 遍历所有的UICheckBox并将它们的Checked属性设置为false
+            foreach (Control control in this.Controls)
+            {
+                if(control is UICheckBox)
+                {
+                    UICheckBox checkBox = control as UICheckBox;
+                    checkBox.Checked = false;
+                }
+            }
+        }
+    }
+}
